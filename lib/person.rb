91f1e1b7bd435d67339a06cc454540a9bea5ede7
Christian V <viray.christianj@gmail.com>
2019-07-26 05:20:43 +0000

    Automatically backed up by Learn

--- lib/person.rb
+++ lib/person.rb
@@ -5,6 +5,17 @@ class Person
   def initialize(name: )
     @name = name 
     @birthday = birthday
-    @hair_color = hair color 
+    @hair_color = hair_color 
+    @eye_color = eye_color
+    @height = height
+    @weight = weight
+    @handed = handed
+    @complexion = complexion
+    @t_shirt_size = t_shirt_size
+    @wrist_size = wrist_size
+    @glove_size = glove_size
+    @pant_length = pant_length
+    @pant_width = pant_width
+  end 
   
 end
